a9ddd6257ba2d4b5290342c01515035d4e4fad1f
jbbadanguio <j.badanguio@gmail.com>
2025-08-25 17:41:42 -0700

    Update udacity_learning.sql
    added subquery code

--- udacity_learning.sql
+++ udacity_learning.sql
@@ -1,3 +1,54 @@
 -- Select everything from a table
 SELECT *
 FROM my_table;
+
+-- SUBQUERIES AND TEMPORARY TABLES
+
+-- Find the number of events that occur for each day for each channel.
+-- First, we needed to group by the day and channel. Then ordering by the number of events (the third column) gave us a quick way to answer the first question.
+SELECT DATE_TRUNC('day',occurred_at) AS day,
+       channel, COUNT(*) as events
+FROM web_events
+GROUP BY 1,2
+ORDER BY 3 DESC;
+
+-- Find the average number of events for each channel.
+-- Here you can see that to get the entire table in question 1 back, we included an * in our* SELECT* statement. You will need to be sure to alias your table.
+SELECT *
+FROM (SELECT DATE_TRUNC('day',occurred_at) AS day,
+                channel, COUNT(*) as events
+          FROM web_events 
+          GROUP BY 1,2
+          ORDER BY 3 DESC) sub;
+
+-- Finally, here we are able to get a table that shows the average number of events a day for each channel.
+SELECT channel, AVG(events) AS average_events
+FROM (SELECT DATE_TRUNC('day',occurred_at) AS day,
+                channel, COUNT(*) as events
+         FROM web_events 
+         GROUP BY 1,2) sub
+GROUP BY channel
+ORDER BY 2 DESC;
+
+-- Pull the month level information about the first order ever placed in the orders table.
+-- Here is the necessary quiz to pull the first month/year combo from the orders table.
+SELECT DATE_TRUNC('month', MIN(occurred_at)) 
+FROM orders;
+
+-- Use the result of the previous query to find only the orders that took place in the same month and year as the first order, and then pull the average for each type of paper qty in this month.
+-- Then to pull the average for each, we could do this all in one query, but for readability, I provided two queries below to perform each separately.                  
+SELECT AVG(standard_qty) avg_std, AVG(gloss_qty) avg_gls, AVG(poster_qty) avg_pst
+FROM orders
+WHERE DATE_TRUNC('month', occurred_at) = 
+     (SELECT DATE_TRUNC('month', MIN(occurred_at)) FROM orders);
+
+SELECT SUM(total_amt_usd)
+FROM orders
+WHERE DATE_TRUNC('month', occurred_at) = 
+      (SELECT DATE_TRUNC('month', MIN(occurred_at)) FROM orders);
+
+
+-- SUBQUERY MANIA
+
+-- Provide the name of the sales_rep in each region with the largest amount of total_amt_usd sales.
+
