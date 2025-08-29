7cfc818440048a5c568b7a32bba399a13a0eb94b
jbbadanguio <j.badanguio@gmail.com>
2025-08-28 17:02:04 -0700

    Update udacity_learning.sql
    Subquery questions 1-3

--- udacity_learning.sql
+++ udacity_learning.sql
@@ -51,4 +51,109 @@ WHERE DATE_TRUNC('month', occurred_at) =
 -- SUBQUERY MANIA
 
 -- Provide the name of the sales_rep in each region with the largest amount of total_amt_usd sales.
+-- ANSWER: 
 
+SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
+FROM sales_reps s
+JOIN accounts a
+ON a.sales_rep_id = s.id
+JOIN orders o
+ON o.account_id = a.id
+JOIN region r
+ON r.id = s.region_id
+GROUP BY 1,2
+ORDER BY 3 DESC; -- lists each rep, region, their total sales and order count
+
+SELECT region_name, MAX(total_amt) total_amt
+        FROM(SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
+                FROM sales_reps s
+                JOIN accounts a
+                ON a.sales_rep_id = s.id
+                JOIN orders o
+                ON o.account_id = a.id
+                JOIN region r
+                ON r.id = s.region_id
+                GROUP BY 1, 2) t1
+        GROUP BY 1; -- Max for each region
+
+SELECT t3.rep_name, t3.region_name, t3.total_amt
+FROM(SELECT region_name, MAX(total_amt) total_amt
+        FROM(SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
+                FROM sales_reps s
+                JOIN accounts a
+                ON a.sales_rep_id = s.id
+                JOIN orders o
+                ON o.account_id = a.id
+                JOIN region r
+                ON r.id = s.region_id
+                GROUP BY 1, 2) t1
+        GROUP BY 1) t2
+JOIN (SELECT s.name rep_name, r.name region_name, SUM(o.total_amt_usd) total_amt
+        FROM sales_reps s
+        JOIN accounts a
+        ON a.sales_rep_id = s.id
+        JOIN orders o
+        ON o.account_id = a.id
+        JOIN region r
+        ON r.id = s.region_id
+        GROUP BY 1,2
+        ORDER BY 3 DESC) t3
+ON t3.region_name = t2.region_name AND t3.total_amt = t2.total_amt; -- join both tables above
+
+-- For the region with the largest (sum) of sales total_amt_usd, how many total (count) orders were placed?
+-- ANSWER: Northeast, Sales: 7744405.36, Order count: 2357
+
+select region, sum(total_sales) sales
+from (select s.name rep, r.name region, sum(o.total_amt_usd) total_sales, count(o.id) order_count
+from sales_reps s
+join region r
+on s.region_id=r.id
+join accounts a
+on s.id=a.sales_rep_id
+join orders o
+on a.id=o.account_id
+group by rep, region
+order by total_sales desc) rep_sales
+group by region
+order by sales desc
+; -- sales by region
+
+-- How many accounts had more total purchases than the account name which has bought the most standard_qty paper throughout their lifetime as a customer?
+
+SELECT a.name account_name, SUM(o.standard_qty) total_std, SUM(o.total) total
+FROM accounts a
+JOIN orders o
+ON o.account_id = a.id
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 1; -- account name which bought the most standard paper = Core-Mark Holding
+
+SELECT a.name
+FROM orders o
+JOIN accounts a
+ON a.id = o.account_id
+GROUP BY 1
+HAVING SUM(o.total) > (SELECT total 
+                      FROM (SELECT a.name act_name, SUM(o.standard_qty) tot_std, SUM(o.total) total
+                            FROM accounts a
+                            JOIN orders o
+                            ON o.account_id = a.id
+                            GROUP BY 1
+                            ORDER BY 2 DESC
+                            LIMIT 1) sub); -- accounts with more total sales
+
+SELECT COUNT(*)
+FROM (SELECT a.name
+          FROM orders o
+          JOIN accounts a
+          ON a.id = o.account_id
+          GROUP BY 1
+          HAVING SUM(o.total) > (SELECT total 
+                      FROM (SELECT a.name act_name, SUM(o.standard_qty) tot_std, SUM(o.total) total
+                            FROM accounts a
+                            JOIN orders o
+                            ON o.account_id = a.id
+                            GROUP BY 1
+                            ORDER BY 2 DESC
+                            LIMIT 1) inner_tab)
+                ) counter_tab; -- lists all accounts with more total orders with counter
